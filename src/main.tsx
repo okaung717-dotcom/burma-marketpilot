d07aad8f227aa26f275483205d46710f4b6379f8
okaung717-dotcom <okaung717@gmail.com>
2026-07-06 00:51:21 +0630

    Load signup guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './signupPasswordGuard';
 import './App.css';
 import './SignupEnhancements.css';
 import './SignupTypography.css';
