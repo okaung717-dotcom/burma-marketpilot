2391c2e6556bd96b5666c19f19cd7a285d70a630
okaung717-dotcom <okaung717@gmail.com>
2026-07-06 17:19:57 +0630

    Import dashboard viewport fit CSS

--- src/DashboardExperience.tsx
+++ src/DashboardExperience.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react';
 import type { BusinessDraft, SetupDraft } from './types';
 import './DashboardExperience.css';
 import './GlobalThemeFixes.css';
+import './DashboardViewportFit.css';
 
 type CalendarRow = {
   date: string;
